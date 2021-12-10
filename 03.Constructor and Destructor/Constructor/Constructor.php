1d3a8a36c3b3f282199f8afbb54304f61372ff39
faisalahammed044274 <setu2014@gmail.com>
2021-12-10 18:37:38 +0600

    Constractor

--- 03.Constructor and Destructor/Constructor/Constructor.php
+++ 03.Constructor and Destructor/Constructor/Constructor.php
@@ -1,18 +1,47 @@
 <?php
+
+/*
+====================
+What is Constructor :
+====================
+
+# Every class has a Magic Method which gets called automatically
+# Constructor are magic methods that gets loaded automatically when instance
+of class is created.
+# Purpose of constructor is to initialize the properties of the class.
+
+===========================
+Purpose of Constructor :
+===========================
+
+# Initialize the properties of class.
+# Initialize the database connection.
+# Check the file Exists.
+# Open the file before using it with the Methods.
+# Check for the internet connection
+# Check API is reachable before the connection
+# Load a Instance of a class.
+# Load mendatory properties to execute the methods in the class.
+
+
+*/
+// Define and Call Constructor
 class Student {
 
-    public $name = "Jhon, Smith";
-    public $class = "1st Std";
+    public $name = "Faisal, Ahammmed";
+    public $class = "PHP OOP Class";
+    public $rollno = "ROll number is 589934";
 
     // if commented this method then we get the value in the result.
     function __construct() //it's a magic method
     {
         $this->name = "";
         $this->class = "";
+        $this->rollno = "";
     }
 
     function printDetails() {
-        echo "$this->name is studying in $this->class class." . PHP_EOL;
+        echo "$this->name is studying in $this->class class and roll number is $this->rollno." . PHP_EOL;
     }
 }
 //Creation of Instance of Class
