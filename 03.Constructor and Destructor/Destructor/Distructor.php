3ad4f62ff425aab73f4cf4e02f0db1848c9217b8
faisalahammed044274 <setu2014@gmail.com>
2021-12-10 20:20:07 +0600

    Distructor

--- 03.Constructor and Destructor/Destructor/Distructor.php
+++ 03.Constructor and Destructor/Destructor/Distructor.php
@@ -1,2 +1,19 @@
-<?php
-/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Distructor</title>
+</head>
+<body>
+    <ul style="list-style: none; background-color:honeydew; margin:20px; padding: 50px; border:3px solid green">
+        <li><h3>What is Distructor ?</h3></li>  <br/>
+        <li>01. Distructor are also magic methods that gets loaded automatically.</li><br/>
+        <li>02. Purpose of Distructor is to do cleanup activities.
+            Example : closing Database connection or saving file.
+        </li><br/>
+        <li>03. You can choose to define Distructor method or ignore it as it is optional to define.</li><br/>
+    </ul>
+</body>
+</html>
