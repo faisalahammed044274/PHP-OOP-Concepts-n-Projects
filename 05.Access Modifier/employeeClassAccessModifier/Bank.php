e63afc8be58ee08aa7d6bad2fc5d415f9b1c9608
faisalahammed044274 <setu2014@gmail.com>
2021-12-15 20:33:22 +0600

    PHP OOP

--- 05.Access Modifier/employeeClassAccessModifier/Bank.php
+++ 05.Access Modifier/employeeClassAccessModifier/Bank.php
@@ -2,35 +2,39 @@
 // 1) Write a Bank Class
 // 2) Define balance as Properties and hide it so that is cannot be accessed via object.
 // 3) Define Public Methods
-// 	- getBalance
-// 	- depositAmount
-// 	- deductAmount
+//     - getBalance
+//     - depositAmount
+//     - deductAmount
 // 4) Set the Initial Balance via the Construct
 
-
-class Bank{
+class Bank
+{
 
     private $balance;
 
-    function getBalance(){
+    public function getBalance()
+    {
         return $this->balance;
     }
 
-    function __construct($balance){
+    public function __construct($balance)
+    {
         $this->balance = $balance;
     }
 
-    function depositAmount($amt){
+    public function depositAmount($amt)
+    {
         $this->balance = $this->balance + $amt;
     }
 
-    function deductAmount($amt){
-        if ($this->balance <= 0){
+    public function deductAmount($amt)
+    {
+        if ($this->balance <= 0) {
             echo "No Balance in the Amount." . PHP_EOL;
             return;
         }
 
-        if($this->balance < $amt){
+        if ($this->balance < $amt) {
             echo "Requested amount is greater then Balance." . PHP_EOL;
             return;
         }
@@ -39,12 +43,9 @@ class Bank{
     }
 }
 
-
 $bank1 = new Bank(100);
 echo "Starting Amount : " . $bank1->getBalance() . PHP_EOL;
 $bank1->depositAmount(700);
 echo "Deposit Amount : " . $bank1->getBalance() . PHP_EOL;
 $bank1->deductAmount(400);
 echo "Current Balance : " . $bank1->getBalance() . PHP_EOL;
-
-?>
